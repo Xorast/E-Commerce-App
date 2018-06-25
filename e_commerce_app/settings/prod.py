24f4dbf2e2ea1f6386ecdc197b84eb7cf25d2d89
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-25 15:55:39 +0000

    AWS storage implemented

--- e_commerce_app/settings/prod.py
+++ e_commerce_app/settings/prod.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 
 SECRET_KEY      = os.environ.get("SECRET_KEY")
 
@@ -6,15 +7,22 @@ DEBUG           = True
 
 ALLOWED_HOSTS   = ["xa-django-e-commerce.herokuapp.com"]
 
-DATABASES       = {}
+DATABASES = {
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
+AWS_STORAGE_BUCKET_NAME     = os.environ.get("AWS_STORAGE_BUCKET_NAME")
+AWS_ACCESS_KEY_ID           = os.environ.get("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY       = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+AWS_S3_HOST                 = 's3-eu-west-1.amazonaws.com'
+AWS_S3_CUSTOM_DOMAIN        = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-STATIC_URL       = ''
-STATICFILES_DIRS = ''
-STATIC_ROOT      = ''
+MEDIAFILES_LOCATION         = 'media'
+MEDIA_URL                   = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE        = 'custom_storages.MediaStorage'
 
-MEDIA_URL        = ''
-MEDIA_ROOT       = ''
+STATICFILES_DIRS            = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_LOCATION        = 'static'
+STATICFILES_STORAGE         = 'custom_storages.StaticStorage'
+STATIC_URL                  = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
