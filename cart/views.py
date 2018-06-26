f64b648cf70bd8bd3ab71ae07ae9cf5639795141
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-26 15:28:19 +0000

    Cart session implemented

--- cart/views.py
+++ cart/views.py
@@ -9,7 +9,18 @@ def display_content(request):
     return render(request, 'cart/cart-view.html')
     
 def cart_item_added(request):
-    id = request.POST["product_id"]
-    product = get_object_or_404(Product, pk=id)
-    return HttpResponse("Item added to cart :" + product.name)
     
+    id          = request.POST["product_id"]
+    product     = get_object_or_404(Product, pk=id)
+    
+    # Get the current Cart
+    cart        = request.session.get('cart', {})
+    
+    # Update the Cart
+    cart[id]    = cart.get(id,0) + 1
+    
+    # Save the Cart back to the session
+    request.session['cart'] = cart
+    
+    # Redirect somewhere
+    return HttpResponse(cart[id])
