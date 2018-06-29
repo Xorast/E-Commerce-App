e536195073b213853aae42264a31dff1d0c2e7c2
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-29 11:04:04 +0000

    Checkout payment test implemented

--- e_commerce_app/settings/base.py
+++ e_commerce_app/settings/base.py
@@ -99,3 +99,5 @@ USE_TZ          = True
 
 STRIPE_PUBLISHABLE_KEY  = os.environ.get("STRIPE_PUBLISHABLE_KEY")
 STRIPE_SECRET_KEY       = os.environ.get("STRIPE_SECRET_KEY")
+
+MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
